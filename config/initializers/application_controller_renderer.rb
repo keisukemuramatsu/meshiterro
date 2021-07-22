4fd20bf2fb36787c402e6563cb7b4ba31871f8ba
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-07-22 08:37:25 +0000

    [Add]投稿フォーム

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ac502e2f18dad4ee652792bc440eff7306559b32c8c25f062597df05605a30754e5d507dc33fe042dd82df5f786cc8f885bb7120165e45d08862194d79b29400'
